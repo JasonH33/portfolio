5492f860ca1d438c6b691660df1e0140d2fdadef
Jason Antonio <jhornet33@outlook.com>
2023-02-02 16:24:20 -0300

    Menu

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+// Opens Menu
+const logo = document.querySelector('#logo')
+const nav = document.querySelector('div.menu')
+const toggle = document.querySelectorAll('.toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('hide')
+    logo.classList.toggle('big')
+  })
+}
+
+// Toggle Menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('hide')
+  })
+}
