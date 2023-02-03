77d153304523266b1dfab251a539bbe3ba18417d
Jason Antonio <jhornet33@outlook.com>
2023-02-03 18:52:54 -0300

    feat: add english version

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
 // Opens Menu
 const logo = document.querySelector('#logo')
+const lang = document.querySelector('.lang')
 const nav = document.querySelector('div.menu')
 const toggle = document.querySelectorAll('.toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('hide')
+    lang.classList.toggle('hide')
     logo.classList.toggle('big')
   })
 }
@@ -16,5 +18,6 @@ const links = document.querySelectorAll('nav ul li a')
 for (const link of links) {
   link.addEventListener('click', function () {
     nav.classList.remove('hide')
+    lang.classList.toggle('hide')
   })
 }
